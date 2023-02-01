e5bf4cd22bc3c3489b370977bb77463ace195f3e
AleLoiac <alessandroloiac@gmail.com>
2023-02-01 17:21:01 +0100

    1/02

--- University_Admission.go
+++ University_Admission.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+)
+
+var exam1, exam2, exam3 int
+
+func meanValue(e1, e2, e3 int) float64 {
+	sum := float64(e1 + e2 + e3)
+	mean := sum / 3
+	return mean
+}
+
 func main() {
 
+	fmt.Scan(&exam1, &exam2, &exam3)
+	fmt.Println(meanValue(exam1, exam2, exam3))
+
+	fmt.Println("Congratulations, you are accepted!")
 }
