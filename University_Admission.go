fe1332956e24525afbafebb4fb4bc2dd54f3db4c
AleLoiac <alessandroloiac@gmail.com>
2023-02-01 17:35:42 +0100

    1/02

--- University_Admission.go
+++ University_Admission.go
@@ -8,7 +8,7 @@ var exam1, exam2, exam3 int
 var score float64
 
 func isAccepted(meanScore float64) {
-	if meanScore > 60.0 {
+	if meanScore >= 60.0 {
 		fmt.Println("Congratulations, you are accepted!")
 	} else {
 		fmt.Println("We regret to inform you that we will not be able to offer you admission.")
