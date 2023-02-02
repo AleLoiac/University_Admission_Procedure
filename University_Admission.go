9915875f7fe29cd7015143fbdeae682c4eabf738
AleLoiac <alessandroloiac@gmail.com>
2023-02-02 14:25:46 +0100

    2/02

--- University_Admission.go
+++ University_Admission.go
@@ -14,20 +14,6 @@ type Student struct {
 	GPA       float64
 }
 
-func isAccepted(meanScore float64) {
-	if meanScore >= 60.0 {
-		fmt.Println("Congratulations, you are accepted!")
-	} else {
-		fmt.Println("We regret to inform you that we will not be able to offer you admission.")
-	}
-}
-
-func meanValue(e1, e2, e3 int) float64 {
-	sum := float64(e1 + e2 + e3)
-	mean := sum / 3
-	return mean
-}
-
 func newStudent(firstname string, lastname string, gpa float64) Student {
 	s := Student{firstName: firstname}
 	s.lastname = lastname
